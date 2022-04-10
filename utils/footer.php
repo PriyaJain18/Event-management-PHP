5a578a13b97c78441b749dd736dd34aa1006be1a
PriyaJain18 <preeyajain09@gmail.com>
2022-04-10 17:18:54 +0530

    first

---
+++ utils/footer.php
@@ -0,0 +1,44 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                    <strong>S</strong><span class = "small footerSubtext">tarry</span>
+
+                    <strong>B</strong><span class = "small footerSubtext">istro</span>
+                        <strong>N</strong><span class = "small footerSubtext">ights</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                    Z-83,Andheri West, Mumbai, Maharashtra 400061 <br>
+                        &copy; Urban Events Venue & Catering, 2022.
+                    <br>Contact details: 8875521333
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    Headquarters : <br>
+                    z-83, Andheri West, Mumbai, Maharashtra 400061<br>
+
+                    <br>
+                    Other Locations : <br>
+                    F-4,7-Block Koramangla,Bengaluru, Karnataka <br>
+                    JK-25, Connaught Place, New Delhi 
+                    
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
